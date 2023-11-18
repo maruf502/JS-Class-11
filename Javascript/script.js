a6c485886c32463670a1080f1abb0589987b7af6
Maruf Khan <marufatrai12@gmail.com>
2023-11-18 11:04:57 +0600

    JS-Class-11

---
+++ Javascript/script.js
@@ -0,0 +1,27 @@
+
+
+
+
+//  Fahrenheit Check mini Project .
+
+    var fahrenheit = prompt ("Enter Fahrenheit = ");
+
+    var CelsiusResult = (fahrenheit-32) * (5 / 9);
+
+    document.write(CelsiusResult);
+
+
+
+
+
+        //  Celsius Check mini Project .
+
+/*
+
+    var Celsius = prompt("Enter Clesius = ");
+
+    var FahrenheitResult = (Celsius * (9 / 5)) +32 ;
+
+    document.write(resultFahrenheit);
+
+*/
